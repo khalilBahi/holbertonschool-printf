3989fb78b3c500c98589b493d6ea18fae03fe82c
sebteouii <sebteouiikhoubayb@gmail.com>
2023-11-24 00:30:55 +0100

    func u need to print s,c

--- fun.c
+++ fun.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_character - printf character
- * Return: -1,0
+ * Return: -1,1
  */
 
 int print_character(va_list arg)
@@ -72,47 +72,6 @@ int print_integer(va_list argument)
         last_digit = r % 10;
         _putchar(last_digit + '0');
         r = r / 10;
-    }
-
-    return (0);
-}
-#include "main.h"
-/**
- * print_character - printf function
- * Return: -1,0
- */
-
-int print_character(va_list arg)
-{
-
-    int ch = va_arg(arg, int);
-
-    if (ch < 0 || ch > 127)
-    {
-        return (-1);
-    }
-
-    _putchar(ch);
-    return (0);
-}
-/**
- * print_string - printf function
- * Return: void
- */
-
-int print_string(va_list arg)
-{
-    int i;
-    char *str = va_arg(arg, char *);
-
-    if (str == NULL)
-    {
-        return (-1);
-    }
-
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(str[i]);
     }
 
     return (0);
